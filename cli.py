c0b18db64f21779632e0d41bd800f962f3ffac8f
Andrea Marcucci <andreamrccc@gmail.com>
2026-07-17 14:17:48 +0200

    Working on dangerous commands

--- cli.py
+++ cli.py
@@ -1,20 +1,31 @@
 from query import connect
+from subprocess import run
+
+def check_command(command: str = None):
+    if not command: 
+        return False
 
 def main():
     while True:
-        user_question = input("q: ").strip()
-        
-        if user_question.lower() == "exit":
-            return
-        
         try: 
-            response = connect(user_question)
-            if response: 
-                print(response)
-            
+            user_question = input("q: ").strip()
         
-        except Exception as e: 
-            print(f"Error: {e}")
+            if user_question.lower() == "exit":
+                return
+        
+            try: 
+                response = connect(user_question)
+                if response: 
+                    print(response)
+
+                choice = input("Want to run this command? (y/N): ").lower() or "n"
+                if choice == "y":
+                    run(response.split())
+        
+            except Exception as e: 
+                print(f"Error: {e}")
+        except KeyboardInterrupt: 
+            return
 
 if __name__ == "__main__":
     main()
